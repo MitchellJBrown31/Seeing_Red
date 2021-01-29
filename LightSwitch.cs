29f713f9cf291f34f478df22ae9f5eb79aad2b69
MitchellJBrown31 <46823222+MitchellJBrown31@users.noreply.github.com>
2021-01-29 15:44:11 -0500

    Add files via upload
    Allows the player to switch off light (eventually with a key press 'E'). Activates enemy flashlights of specific enemies after a specific time (each dictated in inspector).

---
+++ LightSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+
+    public GameObject target;
+    public List<GameObject> flashlightList;
+    public float flashlightDelay;
+
+
+    private bool unused = true, dark = false;
+    private float count = 0;
+
+
+    void OnTriggerStay()
+    {
+        if (unused)
+        {
+            //enable UI
+            if (/*player presses E*/ true )
+            {
+                target.SetActive(false);
+
+                dark = true;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(dark)
+        {
+            count += Time.deltaTime;
+            if(count >= flashlightDelay)
+            {
+                dark = false;
+                flashlightList.ForEach(i =>
+               {
+                   i.SetActive(true);
+               });
+            }
+
+        }
+    }
+}
